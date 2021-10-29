824071ec96d4b7e1726415167bf49b48f8cfc113
Leonardo Setubal <setubal43@gmail.com>
2021-10-28 22:17:47 -0300

    chore: removido elementos nao utilizados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,9 @@
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import {  NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IonicModule } from '@ionic/angular';
-import { GoogleChartsModule } from 'angular-google-charts';
 
 @NgModule({
   declarations: [
