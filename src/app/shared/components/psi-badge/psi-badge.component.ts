b1bd32caaa93de2f4085a36101c89eb95864c0fb
Leonardo Setubal <setubal43@gmail.com>
2021-11-11 01:19:42 -0300

    feat: adicionado novas funcionalidades no badge

--- src/app/shared/components/psi-badge/psi-badge.component.ts
+++ src/app/shared/components/psi-badge/psi-badge.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'psi-badge',
@@ -6,12 +6,25 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./psi-badge.component.scss']
 })
 export class PsiBadgeComponent implements OnInit {
+  @Input() icon?: string
   @Input() label?: string
+  @Input() index?: number
   @Input() color?: string = 'primary'
-  @Input() size?: string = 'size-13'
-  constructor() { }
+  @Input() size?:  string = 'size-13'
+  @Input() cursor?: string = 'default'
+  @Input() animate?: string = 'notAnimete'
+
+  @Output() onClick: EventEmitter<any> = new EventEmitter()
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  onClickEvent(event:Event) {
+    if(this.icon) {
+    this.onClick.emit({event:event, index:this.index})
+    }
+  }
 }
--- src/app/shared/components/psi-badge/psi-badge.module.ts
+++ src/app/shared/components/psi-badge/psi-badge.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PsiBadgeComponent } from './psi-badge.component';
 
@@ -13,6 +13,9 @@ import { PsiBadgeComponent } from './psi-badge.component';
   ],
   exports:[
     PsiBadgeComponent
+  ],
+  schemas:[
+    CUSTOM_ELEMENTS_SCHEMA
   ]
 })
 export class PsiBadgeModule { }
