a9e5b76a1712c9a974f141dfe8b720b125bcc9d4
Leonardo Setubal <setubal43@gmail.com>
2021-11-11 01:25:46 -0300

    lint

--- src/app/modules/articles/search/search.component.ts
+++ src/app/modules/articles/search/search.component.ts
@@ -1,101 +1,116 @@
-import { ChangeDetectorRef, Component, EventEmitter, OnInit, Output, ViewChild, ElementRef } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { SearchAttribute } from 'src/app/shared/enums/SearchAttribute.enum';
 import { IOptionSearch } from 'src/app/shared/interfaces/search.interfaces';
 import { OPTIONS_SEARCH } from './search.const';
 
-
 @Component({
   selector: 'psi-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  styleUrls: ['./search.component.scss'],
 })
 export class SearchComponent implements OnInit {
-
   @ViewChild('simpleInput') simpleInput!: ElementRef;
   @ViewChild('compostInput') compostInput!: ElementRef;
 
-  @Output() search_init = new EventEmitter()
+  @Output() search_init = new EventEmitter();
   private bounceInput!: ReturnType<typeof setTimeout>;
-  public searchInput: FormControl = new FormControl()
-  public inputSelected?: any
-  public arrayInputs: any[] = []
-  public alternativas = OPTIONS_SEARCH
+  public searchInput: FormControl = new FormControl();
+  public inputSelected?: any;
+  public arrayInputs: any[] = [];
+  public alternativas = OPTIONS_SEARCH;
   constructor(private ref: ChangeDetectorRef) {}
 
   ngOnInit(): void {
-    this.search_init.emit(true)
+    this.search_init.emit(true);
   }
 
-  selectInputType(event:KeyboardEvent){
-     this.inputSelected = this.alternativas.find((option)=>{
-      return option.key == this.searchInput.value
-    })
+  selectInputType(event: KeyboardEvent) {
+    this.inputSelected = this.alternativas.find((option) => {
+      return option.key == this.searchInput.value;
+    });
     this.ref.detectChanges();
-    this.compostInput?.nativeElement.focus()
-    if(event.key === 'Enter' && !this.inputSelected){
-      console.log(`adicionar caso não seja por datalist condição para formatar apartir de uma string unica o objeto`)
-    }
-
-  }
-
-inputHandler(event:KeyboardEvent){
-  console.log(event)
-  if(event.key === 'Enter' && this.inputSelected[this.inputSelected?.key].length > 0){
-    this.changeStatusSearchOptions(this.inputSelected.id)
-    this.arrayInputs.push(this.inputSelected)
-    this.resetForms()
-  }
-  if(event.key === 'Backspace' && this.inputSelected[this.inputSelected?.key].length === 0){
-    this.changeStatusSearchOptions(this.inputSelected.id)
-    this.resetForms()
-  }
-}
-
-searchInit(event:KeyboardEvent){
-    clearInterval(this.bounceInput)
-    if((this.searchInput.value.length || this.searchInput.touched) || this.arrayInputs.length >= 1){
-        this.bounceInput = setTimeout(() => {
-          this.search_init.emit(true)
-          this.pesquisar()
-        }, 1500);
-      console.log(this.searchInput)
+    this.compostInput?.nativeElement.focus();
+    if (event.key === 'Enter' && !this.inputSelected) {
+      console.log(
+        `adicionar caso não seja por datalist condição para formatar apartir de uma string unica o objeto`
+      );
     }
   }
 
-  pesquisar(){
+  inputHandler(event: KeyboardEvent) {
+    console.log(event);
+    if (
+      event.key === 'Enter' &&
+      this.inputSelected[this.inputSelected?.key].length > 0
+    ) {
+      this.changeStatusSearchOptions(this.inputSelected.id);
+      this.arrayInputs.push(this.inputSelected);
+      this.resetForms();
+    }
+    if (
+      event.key === 'Backspace' &&
+      this.inputSelected[this.inputSelected?.key].length === 0
+    ) {
+      this.changeStatusSearchOptions(this.inputSelected.id);
+      this.resetForms();
+    }
   }
 
-  remove(event:any){
-    console.log(event)
-    const old = this.arrayInputs.splice(event.index,1);
-    this.changeStatusSearchOptions(old[0].id)
-    this.clearAttr(old[0].id)
+  searchInit(event: KeyboardEvent) {
+    clearInterval(this.bounceInput);
+    if (
+      this.searchInput.value.length ||
+      this.searchInput.touched ||
+      this.arrayInputs.length >= 1
+    ) {
+      this.bounceInput = setTimeout(() => {
+        this.search_init.emit(true);
+        this.pesquisar();
+      }, 1500);
+      console.log(this.searchInput);
+    }
+  }
+
+  pesquisar() {}
+
+  remove(event: any) {
+    console.log(event);
+    const old = this.arrayInputs.splice(event.index, 1);
+    this.changeStatusSearchOptions(old[0].id);
+    this.clearAttr(old[0].id);
     this.ref.detectChanges();
   }
 
-  clearAttr(id:number){
-    this.alternativas.forEach((option)=>{
-      if(option.id === id){
-        const key = option.key as SearchAttribute
-        option[key] = ''
+  clearAttr(id: number) {
+    this.alternativas.forEach((option) => {
+      if (option.id === id) {
+        const key = option.key as SearchAttribute;
+        option[key] = '';
       }
-    })
+    });
   }
 
-  changeStatusSearchOptions(id:number){
-    this.alternativas.forEach(item=>{
-      if(item.id == id){
-        item.hidden = !item.hidden
+  changeStatusSearchOptions(id: number) {
+    this.alternativas.forEach((item) => {
+      if (item.id == id) {
+        item.hidden = !item.hidden;
       }
-    })
+    });
   }
 
-  resetForms(){
-    this.inputSelected = undefined
-    this.searchInput.setValue('')
+  resetForms() {
+    this.inputSelected = undefined;
+    this.searchInput.setValue('');
     this.ref.detectChanges();
-    this.simpleInput.nativeElement.focus()
+    this.simpleInput.nativeElement.focus();
   }
-
 }
