3e220d235883ff71f459da363b9f3d0af2e3efe0
Leonardo Setubal <setubal43@gmail.com>
2021-11-16 09:57:11 -0300

    fix: scroll bar na pagina de artigos

--- src/app/modules/articles/articles.component.ts
+++ src/app/modules/articles/articles.component.ts
@@ -44,11 +44,10 @@ export class ArticlesComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  ngOnChanges(changes: SimpleChanges): void {
-    console.log(`ngChanges Articles`, changes);
-    //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
-    //Add '${implements OnChanges}' to the class.
-  }
+  ngAfterViewInit() {
+    const scrollCustom = document.querySelector('#scrollCustom');
+    this.styleScrollbars(scrollCustom);
+}
 
   async pesquisar(event: ISearchEvent) {
     this.resetSearchParam();
@@ -124,5 +123,38 @@ export class ArticlesComponent implements OnInit {
       paginacao: this.paginacao,
     }
   }
+  styleScrollbars(elmt: any) {
+    const stylesheet = `
+    ::-webkit-scrollbar {
+      width: 4px;
+      height: 4px;
+    }
+
+    ::-webkit-scrollbar-track {
+      background: var(--psi-color-cinza-step-250);
+    }
+
+    ::-webkit-scrollbar-thumb {
+      background: var(--psi-color-cinza-step-300);
+      border-radius: 15px;
+
+    }
+
+    ::-webkit-scrollbar-thumb:hover {
+      background: rgba(141, 141, 141, 0.727);
+    }
+    `;
+
+    const styleElmt = elmt.shadowRoot.querySelector('style');
+
+    if (styleElmt) {
+      styleElmt.append(stylesheet);
+    } else {
+      const barStyle = document.createElement('style');
+      barStyle.append(stylesheet);
+      elmt.shadowRoot.appendChild(barStyle);
+    }
+  }
+
 
 }
