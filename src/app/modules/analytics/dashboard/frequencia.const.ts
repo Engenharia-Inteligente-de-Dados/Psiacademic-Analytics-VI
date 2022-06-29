2658427c1a2461f514bd4585739e65047c21a5e6
Leonardo Setubal <setubal43@gmail.com>
2022-06-28 21:36:46 -0300

    feat: atualização no titulo para "top 10"

--- src/app/modules/analytics/dashboard/frequencia.const.ts
+++ src/app/modules/analytics/dashboard/frequencia.const.ts
@@ -1,6 +1,6 @@
 export const FREQUENCIAS: any = {
   'TIPOS-TRABALHOS': {
-    Title: 'Frequência de tipos de trabalhos',
+    Title: 'Top 10 Frequência de tipos de Trabalhos',
     Frequencias: [
       {
         termo: 'dissertação',
@@ -39,7 +39,7 @@ export const FREQUENCIAS: any = {
     Loading: false,
   },
   'TITULOS-TRABALHOS': {
-    Title: 'Frequência de Título de trabalhos',
+    Title: 'Top 10 Frequência de Título de Trabalhos',
     Frequencias: [
       {
         termo: 'saúde',
