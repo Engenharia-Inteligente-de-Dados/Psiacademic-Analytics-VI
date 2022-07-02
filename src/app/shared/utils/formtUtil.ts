abeca139558db62dad9625d7ad2e4ab96c094384
Leonardo Setubal <setubal43@gmail.com>
2022-07-02 18:58:03 -0300

    lint

--- src/app/shared/utils/formtUtil.ts
+++ src/app/shared/utils/formtUtil.ts
@@ -18,21 +18,21 @@ export function percorrerArray(
   const value = 1;
   const labels = [];
   const values = [];
- array.forEach((item) => {
-      if ( item[keys[label]] === undefined || item[keys[label]] === null) {
-        item[keys[label]] = defaultMessage;
-      }
-      if (
-        item[keys[value]] === undefined ||
-        item[keys[value]] === null ||
-        item[keys[value]] === ``
-      ) {
-        item[keys[value]] = null;
-      }
-      labels.push(item[keys[label]]);
-      values.push(item[keys[value]]);
-    });
-  return {labels,values};
+  array.forEach((item) => {
+    if (item[keys[label]] === undefined || item[keys[label]] === null) {
+      item[keys[label]] = defaultMessage;
+    }
+    if (
+      item[keys[value]] === undefined ||
+      item[keys[value]] === null ||
+      item[keys[value]] === ``
+    ) {
+      item[keys[value]] = null;
+    }
+    labels.push(item[keys[label]]);
+    values.push(item[keys[value]]);
+  });
+  return { labels, values };
 }
 
 /**
@@ -52,15 +52,15 @@ export function formtData(
   return chartData;
 }
 
-    /***
-	 * @method formatTitle Serve para formatar o titulo do grafico.
-	 * É passado como parametro o titulo do grafico e um array com as substiuições.
-   * Exemplo: formatTitle('Atividade {0}', ['1']) retorna Atividade 1
-   * @param title : recebe o titulo do grafico.
-   * @param values : recebe um array com as substituições.
-	 */
-     export function formatTitle(title: string, values: any[]) {
-      return title.replace(/{(\d+)}/g, (match, number) => {
-        return typeof values[number] != 'undefined' ? values[number] : match;
-      });
-    }
+/***
+ * @method formatTitle Serve para formatar o titulo do grafico.
+ * É passado como parametro o titulo do grafico e um array com as substiuições.
+ * Exemplo: formatTitle('Atividade {0}', ['1']) retorna Atividade 1
+ * @param title : recebe o titulo do grafico.
+ * @param values : recebe um array com as substituições.
+ */
+export function formatTitle(title: string, values: any[]) {
+  return title.replace(/{(\d+)}/g, (match, number) => {
+    return typeof values[number] != 'undefined' ? values[number] : match;
+  });
+}
