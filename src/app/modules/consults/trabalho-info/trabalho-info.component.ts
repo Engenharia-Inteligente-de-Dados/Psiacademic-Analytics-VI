24d9042c43d43d6e2b4d76710618e6cb93db44c9
Leonardo Setubal <setubal43@gmail.com>
2022-07-02 16:27:18 -0300

    feat: adicionado popover no texto

--- src/app/modules/consults/trabalho-info/trabalho-info.component.ts
+++ src/app/modules/consults/trabalho-info/trabalho-info.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter, ElementRef, ViewChild } from '@angular/core';
 import { ITrabalho } from '../../../shared/interfaces/trabalhos.interface';
-
+import { createPopper } from '@popperjs/core';
+import { PopoverPositions } from 'src/app/shared/enums/popoverPositions.enum';
 @Component({
   selector: 'trabalho-info',
   templateUrl: './trabalho-info.component.html',
@@ -9,9 +10,56 @@ import { ITrabalho } from '../../../shared/interfaces/trabalhos.interface';
 export class TrabalhoInfoComponent implements OnInit {
   @Input() trabalho: ITrabalho;
   @Output() dismiss = new EventEmitter<any>();
+  @ViewChild('alertIconRef', { static: false }) alertIconRef: ElementRef;
+  popoverShow = false
+  public popper = document.createElement("div");
 
-  constructor() {}
-  ngOnInit(): void {}
+  constructor(
+    ) {}
+  ngOnInit(): void {
+    this.initPopover()
+  }
+
+  initPopover(){
+    this.popper.innerHTML = `
+    <div id="arrow" data-popper-arrow></div>
+    <div class="bg-white border-0 mr-3 block z-50 font-normal leading-normal text-sm max-w-xs text-left no-underline break-words rounded-lg shadow"">
+    <div>
+      <div class="p-3 mb-0 rounded-t-lg">
+        A classificação desse trabalho é realizada, Conforme dados definidos na base de tados e está sujeito a mudança.
+      </div>
+    </div>
+  </div>`;
+  }
+
+  toggleTooltip(){
+    if(this.popoverShow){
+      this.popoverShow = false;
+      this.destroyPopper();
+    } else {
+      this.popoverShow = true;
+      this.createPoppper();
+    }
+  }
+
+  destroyPopper(){
+    this.popper.parentNode.removeChild(this.popper);
+  }
+  createPoppper(){
+    createPopper(this.alertIconRef.nativeElement, this.popper, {
+      placement: PopoverPositions.Bottom,
+      modifiers: [
+        {
+          name: "offset",
+          options: {
+            offset: [0, 8]
+          }
+        }
+      ]
+    });
+    this.alertIconRef.nativeElement.parentNode.insertBefore(this.popper, this.alertIconRef.nativeElement.nextSibling);
+
+  }
 
   dissmiss(event: any) {
     event.stopPropagation();
