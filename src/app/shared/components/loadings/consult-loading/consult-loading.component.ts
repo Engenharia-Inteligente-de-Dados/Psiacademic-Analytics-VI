1f5c583cc12e85c327967bce19331cbd5726e115
Leonardo Setubal <setubal43@gmail.com>
2022-07-25 23:42:54 -0300

    chore: ajuste spinner e consult loading

--- src/app/shared/components/loadings/consult-loading/consult-loading.component.ts
+++ src/app/shared/components/loadings/consult-loading/consult-loading.component.ts
@@ -1,15 +1,25 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, SimpleChanges, ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'consult-loading',
   templateUrl: './consult-loading.component.html',
   styleUrls: ['./consult-loading.component.scss']
 })
-export class ConsultLoadingComponent implements OnInit {
-  @Input() loading:boolean
-  constructor() { }
+export class ConsultLoadingComponent implements OnInit, OnChanges {
+  @Input() loading:boolean = false
+  constructor(
+    private cdref: ChangeDetectorRef
+  ) { }
 
   ngOnInit(): void {
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+    //Add '${implements OnChanges}' to the class.
+    if(changes.loading) {
+      this.loading = changes.loading.currentValue;
+      this.cdref.detectChanges();
+    }
+  }
 }
