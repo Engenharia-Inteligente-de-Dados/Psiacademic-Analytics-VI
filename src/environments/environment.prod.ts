f908bb7e0a989820ee4cc890b1faf38255bcca3a
Leonardo Setubal <setubal43@gmail.com>
2022-12-03 17:17:27 -0300

    chore: alterado api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 declare var require: any
-export const API = 'https://psiacademic-api.herokuapp.com/v2'
+export const API = 'https://74y128.deta.dev/v2'
 export const OLD_API = "https://api-saude-mental.herokuapp.com/contadores/anos"
 export const environment = {
   production: true,
