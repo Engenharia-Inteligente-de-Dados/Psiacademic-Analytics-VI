edd1d90c5f2ad7ab871cb875cdc0207ebe4e03ab
Leonardo Setubal <setubal43@gmail.com>
2023-06-08 16:13:43 -0300

    fix: alteração de url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 declare var require: any
-export const API = 'https://74y128.deta.dev/v2'//'http://127.0.0.1:8000/v2'
+export const API = 'https://psiacademic.herokuapp.com/v2'//'http://127.0.0.1:8000/v2'
 export const OLD_API = "https://api-saude-mental.herokuapp.com/contadores/anos"
 export const environment = {
   production: false,
