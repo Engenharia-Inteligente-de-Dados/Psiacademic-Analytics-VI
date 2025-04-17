c90118302965f5db2090315f29597f45763c5d13
Leonardo Setubal Maggio <Setubal43@gmail.com>
2025-04-17 00:43:05 -0300

    Atualização de URL (#24)
    * fix: max bugdet
    
    * chore: mudança de API
    
    * feat: alterado url
    
    * fix: removendo div
    
    * fix: url de prod

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -6,30 +6,54 @@ import { environment } from 'src/environments/environment';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'Psiacademic Analytics VI';
   constructor() {
     this.mostrarVersao();
+  }
 
-    //document.getElementsByClassName(`ion-page`)[0].classList.remove(`ion-page`);
+
+  ngAfterViewInit() {
+    const body = document.querySelector('body');
+
+    if (!body) return;
+
+    const observer = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+        mutation.addedNodes.forEach((node) => {
+          if (
+            node instanceof HTMLElement &&
+            node.tagName === 'DIV' &&
+            getComputedStyle(node).position === 'fixed'
+          ) {
+            node.style.display = 'none';
+          }
+        });
+      });
+    });
+
+    observer.observe(body, {
+      childList: true,
+      subtree: true
+    });
   }
 
   mostrarVersao() {
 
-		const versao = environment ? environment.VERSION_APP : '0.0.0';
-		const aplicacao =  'Psiacademic Analytics - Visualização de Informação';
-		const ambiente = environment.production
-			? 'Produção'
-			: 'Homologação';
-		console.log(
-			`%c${aplicacao}`,
-			'color: hsl(221, 100%, 50%); font-size: 15px'
-		);
-		console.log(
-			`%cVersão: ${versao} - ${ambiente}`,
-			'color: hsl(221, 100%, 50%); font-size: 15px'
-		);
-	}
+    const versao = environment ? environment.VERSION_APP : '0.0.0';
+    const aplicacao = 'Psiacademic Analytics - Visualização de Informação';
+    const ambiente = environment.production
+      ? 'Produção'
+      : 'Homologação';
+    console.log(
+      `%c${aplicacao}`,
+      'color: hsl(221, 100%, 50%); font-size: 15px'
+    );
+    console.log(
+      `%cVersão: ${versao} - ${ambiente}`,
+      'color: hsl(221, 100%, 50%); font-size: 15px'
+    );
+  }
 }
 
 
