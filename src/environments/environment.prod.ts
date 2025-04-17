e5a6c53a35c6f655ab7d837193a8611c48d6803b
Leonardo Setubal <setubal43@gmail.com>
2025-04-16 23:48:40 -0300

    feat: alterado url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 declare var require: any
-export const API = 'https://psic-saude-api-1719029c4c08.herokuapp.com'
+export const API = 'https://psiacademic-api-v2-8e2f0e43ff4b.herokuapp.com/'
 export const OLD_API = "https://api-saude-mental.herokuapp.com/contadores/anos"
-export const API_P = 'https://psiacademic-api-v2-8e2f0e43ff4b.herokuapp.com/'
+export const API_P = 'https://psic-saude-api-1719029c4c08.herokuapp.com/'
 //export const API_P = "http://127.0.0.1:8000"
 export const environment = {
   production: true,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 declare var require: any
-export const API = 'https://psic-saude-api-1719029c4c08.herokuapp.com'
+export const API = 'https://psiacademic-api-v2-8e2f0e43ff4b.herokuapp.com/v2'
 export const OLD_API = "https://api-saude-mental.herokuapp.com/contadores/anos"
-export const API_P = 'https://psiacademic-api-v2-8e2f0e43ff4b.herokuapp.com'
+export const API_P = 'https://psic-saude-api-1719029c4c08.herokuapp.com'
 //export const API_P = "http://127.0.0.1:8000"
 export const environment = {
   production: false,
